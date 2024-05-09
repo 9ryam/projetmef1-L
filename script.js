3925d0293dc49850739b07da8862126ea15fe999
9ryam <133700925+9ryam@users.noreply.github.com>
2024-05-09 19:12:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 function randomImage() {
-            var images = [
-                "img/butterfly.png",
-                "img/car.png",
-                "img/cat.png",
-                "img/cow.png",
-                "img/flower.png",
-                "img/tree.png"
-                // Ajoutez ici les noms de fichier de vos images
-            ];
+  var images = [
+      { src: "img/butterfly.png", title: "papillon" },
+      { src: "img/car.png", title: "voiture" },
+      { src: "img/cat.png", title: "chat" },
+      { src: "img/cow.png", title: "vache" },
+      { src: "img/flower.png", title: "fleur" },
+      { src: "img/tree.png", title: "arbre" }
+  ];
 
-            var randomIndex = Math.floor(Math.random() * images.length);
-            var randomImage = images[randomIndex];
-            document.getElementById("randomImage").src = randomImage;
-        }
-        
-        // Exécuter la fonction randomImage() lorsque la page se charge
-        window.onload = randomImage;
+  var randomIndex = Math.floor(Math.random() * images.length);
+  var randomImage = images[randomIndex];
+  document.getElementById("randomImage").src = randomImage.src;
+  document.getElementById("randomImage").title = randomImage.title; 
+}
+
+function checkGuess() {
+  var userGuess = document.getElementById("guessInput").value.toLowerCase();
+  var correctAnswer = document.getElementById("randomImage").title.toLowerCase();
+
+  var resultText = "";
+  if (userGuess === correctAnswer) {
+    resultText = "Correct, vous n'êtes pas un robot :) Bienvenue !";
+  } else {
+    resultText = "Faux :/ Réessayez !" ;
+  }
+  document.getElementById("result").textContent = resultText;
+}
+window.onload = randomImage;
+document.getElementById("submitGuess").addEventListener("click", checkGuess);
+
